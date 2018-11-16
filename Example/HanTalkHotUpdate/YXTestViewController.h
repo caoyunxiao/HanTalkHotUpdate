0fe32acc71a53eb119b85f557b8122f9f3eb419d
曹云霄 <1105938518@qq.com>
2018-11-16 17:32:04 +0800

    1.1.2

---
+++ Example/HanTalkHotUpdate/YXTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  YXTestViewController.h
+//  HanTalkHotUpdate_Example
+//
+//  Created by 曹云霄 on 2018/11/15.
+//  Copyright © 2018 caoyunxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YXTestViewController : UIViewController
+
+@end
---
+++ Example/HanTalkHotUpdate/YXTestViewController.m
@@ -0,0 +1,42 @@
+//
+//  YXTestViewController.m
+//  HanTalkHotUpdate_Example
+//
+//  Created by 曹云霄 on 2018/11/15.
+//  Copyright © 2018 caoyunxiao. All rights reserved.
+//
+
+#import "YXTestViewController.h"
+
+@interface YXTestViewController ()
+
+@end
+
+@implementation YXTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)uiConfigAction {
+	
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
